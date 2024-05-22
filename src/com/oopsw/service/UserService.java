f254c953950eb4a94f0291eb93790f8cae665433
HelloNuri <HelloNuri@gmail.com>
2024-05-22 14:50:19 +0900

    userService 완료

--- src/com/oopsw/service/UserService.java
+++ src/com/oopsw/service/UserService.java
@@ -20,7 +20,7 @@ public class UserService {
 			Class.forName("oracle.jdbc.OracleDriver");
 			String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
 			conn = DriverManager.getConnection(url, "hr", "hr");
-			
+
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
@@ -41,66 +41,92 @@ public class UserService {
 	}
 
 	boolean login(String userId, String password) {
+		boolean result = false;
 		try {
-			new UserDAO(conn).login(userId, password);
+			String encryptedPassword = getEncryptedPassword(userId, password);
+			result = new UserDAO(conn).login(userId, encryptedPassword);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return false;
+		return result;
 	}
-	
-	public String encrypt(String text) throws NoSuchAlgorithmException {
-		  
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        try {
-			md.update(text.getBytes("UTF-8"));
-		} catch (UnsupportedEncodingException e) {
+
+	private String getEncryptedPassword(String userId, String password) {
+		MessageDigest md = null;
+		String saltedPassword = userId + password + "salted";
+		String encryptedPassword = bytesToHex(saltedPassword.getBytes());
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+			md.update(saltedPassword.getBytes("UTF-8"));
+			encryptedPassword = bytesToHex(md.digest());
+		} catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
-        return bytesToHex(md.digest());
-        
-    }
-    private String bytesToHex(byte[] bytes) {
-    	
-        StringBuilder builder = new StringBuilder();
-        for (byte b : bytes) {
-            builder.append(String.format("%02x", b));	            
-        }
-        return builder.toString();
-    }
+		
+		// XXX: DB 샘플데이터 활용을 위해 평문비밀번호 사용중. 서비스 시 아래 줄 삭제.
+		encryptedPassword = password;
+		return encryptedPassword;
+	}
+
+	private String bytesToHex(byte[] bytes) {
+
+		StringBuilder builder = new StringBuilder();
+		for (byte b : bytes) {
+			builder.append(String.format("%02x", b));
+		}
+		return builder.toString();
+	}
 
 	boolean isExistEmail(String userId) {
 		boolean result = false;
-		return result;
+		try {
+			result = new UserDAO(conn).isExistEmailTest(userId);
+		} catch (SQLException e) {
+			// result 기본값 false
+		}
 
+		return result;
 	}
 
-	boolean addUser(String userId, UserVO user) {
+	boolean addUser(UserVO user) throws SQLException {
 		boolean result = false;
+		user.setPassword(getEncryptedPassword(user.getEmail(), user.getPassword()));
+		result = new UserDAO(conn).addUser(user);
+
 		return result;
 
 	}
 
-	UserVO getUser(String userId) {
-		return null;
+	UserVO getUser(String userId) throws SQLException {
+		UserVO user = null;
+		user = new UserDAO(conn).getUser(userId);
+		return user;
 
 	}
 
-	String getNickname(String userId) {
-		return userId;
+	String getNickname(String userId) throws SQLException {
+		return new UserDAO(conn).getNickname(userId);
 
 	}
 
 	boolean updateNickname(String userId, String newNickname) {
 		boolean result = false;
+		try {
+			result = new UserDAO(conn).updateNickname(userId, newNickname);
+		} catch (SQLException e) {
+			// result 기본값 default
+		}
 		return result;
-
 	}
 
 	boolean updatePassword(String userId, String newPassword) {
 		boolean result = false;
+		try {
+			String encryptedPassword = getEncryptedPassword(userId, newPassword);
+			result = new UserDAO(conn).updatePassword(userId, encryptedPassword);
+		} catch (SQLException e) {
+			// result 기본값 default
+		}
 		return result;
-
 	}
 }
--- src/test/com/oopsw/UserServiceTest.java
+++ src/test/com/oopsw/UserServiceTest.java
@@ -2,8 +2,6 @@ package test.com.oopsw;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -14,13 +12,6 @@ public class UserServiceTest {
 	@Test
 	public void test() {
 		UserService service = new UserService();
-		List list = new ArrayList();
-		String[] tmp = new String[3];
-		tmp[0] = "1";
-		tmp[1] = "2";
-		tmp[2] = "3";
-		list.add(tmp);
-		System.out.println(list.get(0)[1]);
 	}
 
 }
