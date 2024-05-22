8e6332d0336267ebacf3005eb89577045ef25220
migang <soon__e_i@naver.com>
2024-05-22 16:36:20 +0900

    chore: reservationService 구현중

--- src/com/oopsw/service/ReservationService.java
+++ src/com/oopsw/service/ReservationService.java
@@ -19,10 +19,10 @@ import com.oopsw.model.vo.ReservationVO;
 import com.oopsw.model.vo.RoomInfoVO;
 
 public class ReservationService {
-	
+
 	public Connection conn;
-	
-	public ReservationService(){
+
+	public ReservationService() {
 		try {
 			Context context = new InitialContext();
 			DataSource dataSource = (DataSource) context.lookup("java:comp/env/jdbc/myoracle");
@@ -31,7 +31,7 @@ public class ReservationService {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// ReservationDAO reservationDAO = null;
 	KKDAO kkDAO = null;
 
@@ -39,30 +39,98 @@ public class ReservationService {
 	public Collection<ReservationVO> getUpcomingReservation(String userId) {
 
 		Collection<ReservationVO> reservationVOs = new ArrayList<>();
-		reservationVOs = reservationDAO.getUpcomingReservation(userId);
+		reservationVOs = new ReservationDAO(conn).getUpcomingReservation(userId);
 		return reservationVOs;
 	}
-	
-	/** 해당 노래방의 방, 예약 현황 정보 불러오기 */
-	public Collection<ReservationVO> getroomReservationStatusList(int kkId) {
 
-		Collection<ReservationVO> reservationVOs = new ArrayList<>();
+	/** 해당 노래방의 방, 예약 현황 정보 불러오기 */
+	public Collection<ReservationRoomInfoVO> getroomReservationStatusList(int kkId) {
+
+		Collection<ReservationRoomInfoVO> reservationRoomInfoVOs = new ArrayList<>();
 		RoomInfoVO roomInfoVO = null;
-//		Map<Integer, String>
-		
+		// Map<Integer, String>
+
 		// roomInfoVO = new KKDAO(conn).getRoomInfoList(kkId);
 		// 리턴값 : room_id, name(방 이름)
 		reservationVOs = new ReservationDAO(conn).getReservationListByRoomId(roomId);
 		// 리턴값 : s.reservation_id, s.start_time, s.end_time
 		// 돌려줄 때 reservationVO + 방 이름 VO가 필요함
-		
+
+		// 정보 받아서 병합
+
 		reservationVOs = reservationDAO(conn).getUpcomingReservation(userId);
+
+		return reservationRoomInfoVOs;
+	}
+
+	/** 예약 가능 시간인지 검증하기 */
+	public boolean isValidTimeForReservation(ReservationVO reservationVO) {
+
+		boolean result = false;
+
+		result = new ReservationDAO(conn).isValidTimeForReservation(reservationVO);
+
+		return result;
+	}
+
+	/** 예약내역 중 이용 중/예정 내역 불러오기 */
+	public Collection<ReservationVO> getUncompletedReservationList(String userId) {
+
+		Collection<ReservationVO> reservationVOs = new ArrayList<>();
+
+		reservationVOs = new ReservationDAO(conn).getUncompletedReservationList(userId);
 
 		return reservationVOs;
 	}
 
-	public Collection<ReservationVO> getUncompletedReservationList() {
-		return null;
+	/** 예약내역 중 이용 완료 내역 불러오기 */
+	public Collection<ReservationVO> getCompletedReservationList(String userId) {
+
+		Collection<ReservationVO> reservationVOs = new ArrayList<>();
+
+		reservationVOs = new ReservationDAO(conn).getCompletedReservationList(userId);
+
+		return reservationVOs;
+	}
+
+	/** 예약내역 중 취소 내역 불러오기 */
+	public Collection<ReservationVO> getCanceledReservationList(String userId) {
+
+		Collection<ReservationVO> reservationVOs = new ArrayList<>();
+
+		reservationVOs = new ReservationDAO(conn).getCanceledReservationList(userId);
+
+		return reservationVOs;
+	}
+
+	/** 기존 이용 시간, 추가 가능 시간 불러오기 */
+	public Collection<ReservationVO> additionalTimeStatus(String userId, int reservationId) {
+
+		Collection<ReservationVO> reservationVOs = new ArrayList<>();
+		
+		ReservationVO reservationVO = null;
+		
+		reservationVO = new ReservationDAO(conn).getOriginalReservationTime(userId, reservationId);
+
+
+		return reservationVOs;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	/** 예약 취소하기 */
+	public boolean cancelReservation(int reservationId) {
+
+		boolean result = false;
+
+		result = new ReservationDAO(conn).cancelReservation(reservationId);
+
+		return result;
 	}
 
 }
