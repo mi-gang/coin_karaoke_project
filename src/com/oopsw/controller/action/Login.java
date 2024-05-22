b547ff9dffc9c8200ba292b90d05ad9f9e25fb44
HelloNuri <HelloNuri@gmail.com>
2024-05-22 16:35:33 +0900

    loginAction 테스트 직전까지 완료.

--- src/com/oopsw/controller/action/Login.java
+++ src/com/oopsw/controller/action/Login.java
@@ -1,15 +1,28 @@
 package com.oopsw.controller.action;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import com.oopsw.controller.Action;
 import com.oopsw.controller.Url;
+import com.oopsw.service.UserService;
 
 public class Login implements Action {
 
 	@Override
 	public Url execute(HttpServletRequest request) {
-		Url url = new Url("controller?cmd=url", Url.REDIRECT);
+		Url url = new Url("controller?cmd=loginUI", Url.FORWARD);
+		String userId = request.getParameter("userId");
+		String password = request.getParameter("password");
+		boolean isValidLogin= new UserService().login(userId, password);
+		
+		if(isValidLogin){
+			HttpSession session = request.getSession();
+			session.setAttribute("userId", userId);
+			url.setUrl("controller?cmd=mainUI");
+			url.setFlag(Url.REDIRECT);
+		}
+		
 		return url;
 	}
 
