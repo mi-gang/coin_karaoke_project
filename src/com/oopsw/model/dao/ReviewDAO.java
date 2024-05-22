2da805f34152b0bf59c517470245724235d6fa95
migang <soon__e_i@naver.com>
2024-05-22 10:58:45 +0900

    feat: reviewDAO, InquireDAO 구현

--- src/com/oopsw/model/dao/ReviewDAO.java
+++ src/com/oopsw/model/dao/ReviewDAO.java
@@ -12,40 +12,62 @@ import com.oopsw.model.vo.ReservationVO;
 import com.oopsw.model.vo.ReviewVO;
 
 public class ReviewDAO {
-    	private Connection conn;
+	private Connection conn;
 
 	public ReviewDAO(Connection conn) {
 		this.conn = conn;
 	}
 
-    // 리뷰애 출력할 정보를 다시 확인할 필요가 있음 ..
-    public Collection<ReviewVO> getReviewListByKKId(int KKId){
-        
-        String sql = "select review_id, content, star, start_time, end_time, name from "
-        + "(SELECT * from reviews rev, reservations res, KKs k where rev.RESERVATION_ID = res.RESERVATION_ID "
-        + "and k.KK_ID = ? order by res.END_TIME desc) where rownum >= 1 and rownum <= 10";
+	// sql 체크 필요
+	/** 노래방 별점 불러오기 */
+	public float getStarAvgByKKId(int KKId) {
 
-        Collection<ReviewVO> reviewVOs = new ArrayList<ReviewVO>();
+		String sql = "select avg(r.star), k.kk_id from reviews r,reservations v,room_infos f,kks k "
+				+ "where r.reservation_id= v.reservation_id AND v.room_id=f.room_id AND f.kk_id=k.kk_id "
+				+ "and kk_id = ? group by k.kk_id";
+
+		float num = 0;
+
+		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setInt(1, KKId);
+
+			num = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return num;
+	}
+
+	// sql 체크 필요
+	/** 해당 노래방 리뷰 불러오기 */
+	public Collection<ReviewVO> getReviewListByKKId(int KKId) {
+
+		String sql = "select review_id, content, nickname from "
+				+ "(SELECT * from reviews rev, reservations res, KKs k where rev.RESERVATION_ID = res.RESERVATION_ID "
+				+ "and k.KK_ID = ? order by res.END_TIME desc) where rownum >= 1 and rownum <= 10";
+
+		Collection<ReviewVO> reviewVOs = new ArrayList<ReviewVO>();
 
 		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setInt(1, KKId);
 			try (ResultSet rs = pstmt.executeQuery()) {
 				while (rs.next()) {
-					reviewVOs.add(new ReservationVO(rs.getInt(1), rs.getTimestamp(2).toLocalDateTime(),
-							rs.getTimestamp(3).toLocalDateTime(), roomInfoVO.getRoomId()));
+					reviewVOs.add(new ReviewVO(rs.getInt(1), rs.getString(2), rs.getFloat(3),
+							rs.getTimestamp(4).toLocalDateTime(), rs.getTimestamp(5).toLocalDateTime(),
+							rs.getString(6)));
 				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return reviewVOs;
-    }
+	}
 
 	/** 리뷰 작성하기 */
 	public boolean addReview(ReviewVO reviewVO) {
 
 		String sql = "INSERT into reviews (review_id, content, star, reservation_id, review_date) "
-		+ "values (review_id_seq.nextval, ?, ?, ?, sysdate)";
+				+ "values (review_id_seq.nextval, ?, ?, ?, sysdate)";
 
 		boolean result = false;
 
@@ -64,7 +86,7 @@ public class ReviewDAO {
 		return result;
 	}
 
-	/**리뷰 삭제하기 */
+	/** 리뷰 삭제하기 */
 	public boolean addReview(int reviewId) {
 
 		String sql = "delete from reviews where review_id = ?";
@@ -85,25 +107,26 @@ public class ReviewDAO {
 	}
 
 	/** 나의 리뷰 리스트 불러오기 */
-	public Collection<ReviewVO> getReviewListByUserId(int userId){
-        
-        String sql = "select review_id, content, star, start_time, end_time, name from "
-			+ "(SELECT * from reviews rev, reservations res, KKs k where rev.RESERVATION_ID = res.RESERVATION_ID "
-			+ "and res.USER_ID = ? order by rev.review_date desc) where rownum > 0 and rownum <= 10";
+	public Collection<ReviewVO> getReviewListByUserId(int userId) {
 
-        Collection<ReviewVO> reviewVOs = new ArrayList<ReviewVO>();
+		String sql = "select review_id, content, star, start_time, end_time, name from "
+				+ "(SELECT * from reviews rev, reservations res, KKs k where rev.RESERVATION_ID = res.RESERVATION_ID "
+				+ "and res.USER_ID = ? order by rev.review_date desc) where rownum > 0 and rownum <= 10";
+
+		Collection<ReviewVO> reviewVOs = new ArrayList<ReviewVO>();
 
 		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setInt(1, userId);
 			try (ResultSet rs = pstmt.executeQuery()) {
 				while (rs.next()) {
-					reviewVOs.add(new ReservationVO(rs.getInt(1), rs.getTimestamp(2).toLocalDateTime(),
-							rs.getTimestamp(3).toLocalDateTime(), roomInfoVO.getRoomId()));
+					reviewVOs.add(new ReviewVO(rs.getInt(1), rs.getString(2), rs.getFloat(3),
+							rs.getTimestamp(4).toLocalDateTime(), rs.getTimestamp(5).toLocalDateTime(),
+							rs.getString(6)));
 				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return reviewVOs;
-    }
+	}
 }
--- src/com/oopsw/model/vo/ReviewVO.java
+++ src/com/oopsw/model/vo/ReviewVO.java
@@ -1,83 +1,141 @@
 package com.oopsw.model.vo;
 
+import java.time.LocalDateTime;
+
 public class ReviewVO {
-    private int reviewId;
-    private String content;
-    private float star;
-    private int reservationId;
+	private int reviewId;
+	private String content;
+	private float star;
+	private int reservationId;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
+	private String KKname;
 
-    
-    public ReviewVO(int reviewId, String content, float star, int reservationId) {
-        setReviewId(reviewId);
-        setContent(content);
-        setStar(star);
-        setReservationId(reservationId);
-    }
-    
-    public int getReviewId() {
-        return reviewId;
-    }
-    public void setReviewId(int reviewId) {
-        this.reviewId = reviewId;
-    }
-    public String getContent() {
-        return content;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
-    public float getStar() {
-        return star;
-    }
-    public void setStar(float star) {
-        this.star = star;
-    }
-    public int getReservationId() {
-        return reservationId;
-    }
-    public void setReservationId(int reservationId) {
-        this.reservationId = reservationId;
-    }
+	public ReviewVO(int reviewId, String content, float star, LocalDateTime startTime, LocalDateTime endTime,
+			String KKname) {
+		setReviewId(reviewId);
+		setContent(content);
+		setStar(star);
+		setReservationId(reservationId);
+		setStartTime(startTime);
+		setEndTime(endTime);
+		setKKname(KKname);
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + reviewId;
-        result = prime * result + ((content == null) ? 0 : content.hashCode());
-        result = prime * result + Float.floatToIntBits(star);
-        result = prime * result + reservationId;
-        return result;
-    }
+	public int getReviewId() {
+		return reviewId;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        ReviewVO other = (ReviewVO) obj;
-        if (reviewId != other.reviewId)
-            return false;
-        if (content == null) {
-            if (other.content != null)
-                return false;
-        } else if (!content.equals(other.content))
-            return false;
-        if (Float.floatToIntBits(star) != Float.floatToIntBits(other.star))
-            return false;
-        if (reservationId != other.reservationId)
-            return false;
-        return true;
-    }
+	public void setReviewId(int reviewId) {
+		this.reviewId = reviewId;
+	}
 
-    @Override
-    public String toString() {
-        return "ReviewVO [reviewId=" + reviewId + ", content=" + content + ", star=" + star + ", reservationId="
-                + reservationId + "]";
-    }
+	public String getContent() {
+		return content;
+	}
 
-    
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public float getStar() {
+		return star;
+	}
+
+	public void setStar(float star) {
+		this.star = star;
+	}
+
+	public int getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(int reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(LocalDateTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getKKname() {
+		return KKname;
+	}
+
+	public void setKKname(String kKname) {
+		KKname = kKname;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((KKname == null) ? 0 : KKname.hashCode());
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((endTime == null) ? 0 : endTime.hashCode());
+		result = prime * result + reservationId;
+		result = prime * result + reviewId;
+		result = prime * result + Float.floatToIntBits(star);
+		result = prime * result + ((startTime == null) ? 0 : startTime.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReviewVO other = (ReviewVO) obj;
+		if (KKname == null) {
+			if (other.KKname != null)
+				return false;
+		} else if (!KKname.equals(other.KKname))
+			return false;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (endTime == null) {
+			if (other.endTime != null)
+				return false;
+		} else if (!endTime.equals(other.endTime))
+			return false;
+		if (reservationId != other.reservationId)
+			return false;
+		if (reviewId != other.reviewId)
+			return false;
+		if (Float.floatToIntBits(star) != Float.floatToIntBits(other.star))
+			return false;
+		if (startTime == null) {
+			if (other.startTime != null)
+				return false;
+		} else if (!startTime.equals(other.startTime))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ReviewVO [reviewId=" + reviewId + ", content=" + content + ", star=" + star + ", reservationId="
+				+ reservationId + ", startTime=" + startTime + ", endTime=" + endTime + ", KKname=" + KKname + "]";
+	}
+
+	//
+
 }
