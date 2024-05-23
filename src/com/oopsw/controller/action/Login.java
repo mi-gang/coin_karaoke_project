a74a442ff3a5e9dc1f7ad31d1bf26674f7b5123f
HelloNuri <HelloNuri@gmail.com>
2024-05-23 11:14:05 +0900

    data.jsp 수정사항 반영하여 loginAction 변경

--- src/com/oopsw/controller/action/Login.java
+++ src/com/oopsw/controller/action/Login.java
@@ -1,30 +1,40 @@
 package com.oopsw.controller.action;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.oopsw.controller.Action;
 import com.oopsw.controller.Url;
+import com.oopsw.model.vo.UserVO;
 import com.oopsw.service.UserService;
 
 public class Login implements Action {
 
 	@Override
 	public Url execute(HttpServletRequest request) {
-		Url url = new Url("controller?cmd=loginUI", Url.FORWARD);
 		String userId = request.getParameter("userId");
 		String password = request.getParameter("password");
-		boolean isValidLogin= new UserService().login(userId, password);
+		boolean loginSuccess = false;
+		loginSuccess = new UserService().login(userId, password);
 		
-		if(isValidLogin){
+		if(loginSuccess){
 			HttpSession session = request.getSession();
 			session.setAttribute("userId", userId);
-			url.setUrl("controller?cmd=mainUI");
-			url.setFlag(Url.REDIRECT);
 			System.out.println("로그인 성공: " + userId);
 		}
 		
-		return url;
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		JsonObject json = new JsonObject();
+		json.addProperty("result", loginSuccess);
+		request.setAttribute("dataToSend", gson.toJson(json));
+		
+		return new Url("json/data.jsp", Url.FORWARD);
 	}
 
 }
